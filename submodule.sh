9acb7b6319efc6ec7ac6b1f41344a217ba98696b
Vallabh Kansagara <vrkansagara@gmail.com>
2021-04-02 22:38:11 +0530

    adding change

--- submodule.sh
+++ submodule.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-# set -x  Used to debug the script
+set -x  Used to debug the script
 
 if [ "$(whoami)" != "root" ]; then
 	SUDO=sudo
@@ -10,17 +10,17 @@ fi
 CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Submodule installation started at ${CURRENT_DATE}"
+echo "Sub-module installation started at ${CURRENT_DATE}"
 
 cd ~/.vim
 
-read -r -p "Do you want to remove git modules ? [Y/n] " input
+read -r -p "Do you want to remove git modules ? [Y/n]" input
 case $input in
     [yY][eE][sS]|[yY])
 		echo "Install desktop manager"
-		${SUDO} rm -rf ./.git/modules/*
-		${SUDO} rm -rf .gitmodules
 		${SUDO} touch .gitmodules
+		${SUDO} rm -rf .gitmodules
+		${SUDO} rm -rf ./.git/modules/*
  ;;
     [nN][oO]|[nN])
  echo "Skipping...XFCE"
@@ -55,6 +55,7 @@ git submodule add -f https://github.com/tpope/vim-surround.git vendor/vim-surrou
 echo "Installation of [ Light & Dark Vim color schemes inspired by Google's Material Design  ] ..."
 git submodule add -f https://github.com/NLKNguyen/papercolor-theme.git vendor/papercolor-theme
 
+# As CtrlP is the 100% vim so no need extra burden of plugin and shell library
 # echo "Installation of [ A command-line fuzzy finder   ] ..."
 # git submodule add -f  https://github.com/junegunn/fzf vendor/fzf
 # echo "Installation of [ fzf heart vim  ] ..."
@@ -64,16 +65,19 @@ echo "Installation of [ lean & mean status/tabline for vim that's light as air  
 git submodule add -f https://github.com/vim-airline/vim-airline.git vendor/vim-airline
 git submodule add -f https://github.com/vim-airline/vim-airline-themes.git vendor/vim-airline-theme
 
-echo "Installation of [ A Vim plugin which shows git diff markers in the sign column and stages/previews/undoes hunks and partial hunks. ] ..."
+echo "Installation of [ A Vim plugin which shows git diff markers in the sign column and 
+stages/previews/undoes hunks and partial hunks. ] ..."
 git submodule add -f https://github.com/airblade/vim-gitgutter.git vendor/vim-gitgutter
 
-echo "Installation of [ Markdown for Vim: a complete environment to create Markdown files with a syntax highlight that doesn't suck!  ] ..."
+echo "Installation of [ Markdown for Vim: a complete environment to create Markdown files with 
+a syntax highlight that doesn't suck!  ] ..."
 git submodule add -f https://github.com/gabrielelana/vim-markdown.git vendor/vim-markdown
 
 echo "Installation of [ A tree explorer plugin for vim. ] ..."
 git submodule add -f https://github.com/preservim/nerdtree.git vendor/nerdtree
 
-# echo "Installation of [ Nodejs extension host for vim & neovim, load extensions like VSCode and host language servers. ] ..."
+# echo "Installation of [ Nodejs extension host for vim & neovim, load extensions like VSCode 
+# and host language servers. ] ..."
 # git submodule add -f https://github.com/neoclide/coc.nvim.git vendor/coc.nvim
 
 #echo "Installation of [ Perform all your vim insert mode completions with Tab  ] ..."
@@ -88,7 +92,8 @@ git submodule add -f https://github.com/MarcWeber/vim-addon-mw-utils.git vendor/
 git submodule add -f https://github.com/garbas/vim-snipmate.git vendor/vim-snipmate
 git submodule add -f https://github.com/honza/vim-snippets.git vendor/vim-snippets
 
-# echo "Installation of [ clap Modern performant generic finder and dispatcher for Vim and NeoVim  ] ..."
+# echo "Installation of [ clap Modern performant 
+# generic finder and dispatcher for Vim and NeoVim  ] ..."
 # git submodule add -f https://github.com/liuchengxu/vim-clap.git vendor/vim-clap
 
 # git submodule add -f https://github.com/jlanzarotta/bufexplorer.git vendor/bufexplorer
