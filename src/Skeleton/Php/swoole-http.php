3aef69a94e1bd95a333ca4a184a20620afaa1848
Vallabh Kansagara <vrkansagara@gmail.com>
2021-04-14 21:46:48 +0530

    adding todo

---
+++ src/Skeleton/Php/swoole-http.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+
+$host = '127.0.0.1';
+$post = 8090;
+$http = new Swoole\HTTP\Server($host,$post);
+
+$http->on('request', function ($request, $response) {
+    $response->end("<h1>Hello World. #".rand(1000, 9999)."</h1>");
+});
+
+echo sprintf("Server listening at %s:%d",$host,$post);
+$http->start();
+
