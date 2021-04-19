7292f8262199101d9ee891d83a0926dd562776d4
Vallabh Kansagara <vrkansagara@gmail.com>
2021-04-19 18:57:09 +0530

    all random

--- src/Sh/System/battery.sh
+++ src/Sh/System/battery.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
 export DEBIAN_FRONTEND=noninteractive
 
-if [ "$(whoami)" != "root" ]; then
+if [ "$(whoami)" != "root" \]; then
 	SUDO=sudo
 fi
+
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note		  :-
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 if ! command -v acpi &> /dev/null
 then
@@ -12,16 +21,24 @@ then
 	${SUDO} apt-get install -y acpi
 fi
 
-while true                                                                                                                                                                              
+while true
 do
 	export DISPLAY=:0.0
 	battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-	if on_ac_power; then                                #check if AC is plugged in
-		if [ $battery_level -ge 99 ]; then              #check if the battery level is over 90%
-			notify-send -u critical "Please unplug your AC adapter" "Battery level: ${battery_level}% (charged above 100%)" -i battery-full-charged
-		fi
-	fi
-	echo "Current battery level is $battery_level"
-	sleep 30                                             #wait for 30 seconds before checking again
-done
 
+	#check if AC is plugged in
+	if on_ac_power; then
+
+		#check if the battery level is over 90%
+		if [ $battery_level -ge 99 ]; then
+			notify-send -u critical "Please unplug your AC adapter" "Battery
+			level: ${battery_level}% (charged above 99%)" -i battery-full-charged
+		fi
+
+	fi
+
+	echo "Current battery level is $battery_level"
+	#wait for 30 seconds before checking again
+	sleep 30
+
+done
