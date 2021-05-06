45924606d6cb0a06fdc739b534dbfea53dfa4b6d
Vallabh Kansagara <vrkansagara@gmail.com>
2021-05-06 20:11:08 +0530

    adding few changes

--- src/Sh/PHP/deb-sury-org.sh
+++ src/Sh/PHP/deb-sury-org.sh
@@ -1,15 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
 export DEBIAN_FRONTEND=noninteractive
 
-if [ "$(whoami)" != "root" ]; then
-    SUDO=sudo
+if [ "(whoami)" != "root" ]; then
+	SUDO=sudo
 fi
+
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note		  :- 
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 ${SUDO} apt-get -y install apt-transport-https lsb-release ca-certificates curl
 ${SUDO} curl -sSL -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 ${SUDO} sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-${SUDO} apt-get update
+${SUDO} apt-get -y update
 
 echo "[DONE] deb-sury-org.sh"
-exit 0
+
+exit 1
