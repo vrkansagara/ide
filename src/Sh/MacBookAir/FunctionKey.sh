446f1b8ac0c5e0810e88bc34f9264ce4528c48b5
Vallabh Kansagara <vrkansagara@gmail.com>
2021-05-11 17:40:04 +0530

    adding random changes

--- src/Sh/MacBookAir/FunctionKey.sh
+++ src/Sh/MacBookAir/FunctionKey.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
 export DEBIAN_FRONTEND=noninteractive
 
-if [ "$(whoami)" != "root" ]; then
+if [ "$(whoami)" != "root" \]; then
     SUDO=sudo
 fi
+
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note       :-
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 echo "This will invert the function key"
 
