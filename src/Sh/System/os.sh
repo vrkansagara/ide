523d81eb6fa34c50a62d66f5cf9c3da71155805a
Vallabh Kansagara <vrkansagara@gmail.com>
2021-05-21 16:44:43 -0700

    adding cgroup config

---
+++ src/Sh/System/os.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
+echo
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$(whoami)" != "root" ]; then
+	SUDO=sudo
+fi
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note		  :- Check the linux os and it's version
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+echo "Linux name     :- $OS"
+echo "Linux version  :- $VER"
