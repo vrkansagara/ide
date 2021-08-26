b53715b40ab424a3be28af6ea74da23c7e50a2c9
Vallabh Kansagara <vrkansagara@gmail.com>
2021-08-26 08:01:38 +0530

    adding jekyll config

---
+++ src/Sh/Ruby/jekyll.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
+
+echo
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$(whoami)" != "root" ]; then
+	SUDO=sudo
+fi
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note		  :- 
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+${SUDO} apt-get install -y ruby-full build-essential zlib1g-dev
+
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+${SUDO} gem install jekyll bundler
+
+${SUDO} gem update --system
+
+# Example
+# jekyll new /tmp/myblog
+# cd /tmp/myblog
+# bundle exec jekyll serve  OR bundle exec jekyll serve --livereload
