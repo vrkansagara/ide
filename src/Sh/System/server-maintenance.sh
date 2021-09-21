3d3735005df0e8d80eb5142d5e758baab24db57a
Vallabh Kansagara <vrkansagara@gmail.com>
2021-09-21 11:02:45 +0530

    adding automation

---
+++ src/Sh/System/server-maintenance.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
+
+echo ""
+export DEBIAN_FRONTEND=noninteractive
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+SCRIPT=$(readlink -f "")
+SCRIPTDIR=$(dirname "$SCRIPT")
+
+if [ "$(whoami)" != "root" ]; then
+	SUDO=sudo
+fi
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Note		  :- 
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+Ref:- https://newbedev.com/disable-all-services-except-ssh
+Disable all services, except ssh
+emergency-net.target
+
+[Unit]
+Description=Maintenance Mode with Networking and SSH
+Requires=maintenance.target systemd-networkd.service sshd.service
+After=maintenance.target systemd-networkd.service sshd.service
+AllowIsolate=yes
+
+# systemctl isolate maintenance.target
+
+# systemctl isolate multi-user.target
+
