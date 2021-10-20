6f8e2b1c4b4fef4b5cac440743d08e43c75903db
Vallabh Kansagara <vrkansagara@gmail.com>
2021-10-20 09:18:43 +0530

    2021 all random

--- src/Sh/gnupg.sh
+++ src/Sh/gnupg.sh
@@ -2,7 +2,7 @@
 # set -e # This setting is telling the script to exit on a command error.
 # set -x # You refer to a noisy script.(Used to debugging)
 
-echo ""
+echo " "
 export DEBIAN_FRONTEND=noninteractive
 CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 SCRIPT=$(readlink -f "")
@@ -17,11 +17,11 @@ fi
 #  Note		  :- 
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-${SUDO} apt-get install gnupg2
+${SUDO} apt-get install -y --reinstall --no-install-recommends gnupg2
 ${SUDO} mkdir $HOME/.gnupg
 # To fix the " gpg: WARNING: unsafe permissions on homedir '/home/path/to/user/.gnupg' " error
 # Make sure that the .gnupg directory and its contents is accessibile by your user.
-${SUDO} chown -R $(whoami) ~/.gnupg/
+${SUDO} chown -R $USER ~/.gnupg/
 
 # Also correct the permissions and access rights on the directory
 ${SUDO} chmod 600 ~/.gnupg/*
@@ -32,21 +32,21 @@ ${SUDO} chmod 700 ~/.gnupg
 
 # gpg --default-new-key-algo rsa4096 --gen-key
 # Generate a new pgp key: (better to use gpg2 instead of gpg in all below commands)
-gpg --gen-key
+# gpg --gen-key
 # maybe you need some random work in your OS to generate a key. so run this command: `find ./* /home/username -type d | xargs grep some_random_string > /dev/null`
 
 # check current keys:
 gpg --list-secret-keys --keyid-format LONG
 
 # See your gpg public key:
-gpg --armor --export YOUR_KEY_ID
+# gpg --armor --export YOUR_KEY_ID
 # YOUR_KEY_ID is the hash in front of `sec` in previous command. (for example sec 4096R/234FAA343232333 => key id is: 234FAA343232333)
 
 # Set a gpg key for git:
-git config --global user.signingkey your_key_id
+# git config --global user.signingkey your_key_id
 
 # To sign a single commit:
-git commit -S -a -m "Test a signed commit"
+# git commit -S -a -m "Test a signed commit"
 
 # Auto-sign all commits globaly
 git config --global commit.gpgsign true
