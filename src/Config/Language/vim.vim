3e2b941c07927e9bba591bce87af3759210ac2d4
Vallabh Kansagara <vrkansagara@gmail.com>
2021-10-22 18:58:28 +0530

    addin dynamic refresh per filetyp using F5 key

---
+++ src/Config/Language/vim.vim
@@ -0,0 +1,7 @@
+" This function is dynamically called by Pressing F5 by (filetype.vim)
+function! RefreshF5vim ()
+    " Call F2 which is trim whitespace for all file type
+    exe "normal \<F2>"
+    " Indent whole file
+    exe "normal gg=G"
+endfunction
--- src/Skeleton/Php/index.php
+++ src/Skeleton/Php/index.php
@@ -1,5 +1,7 @@
 <?php
-               declare(strict_types=1);
+
+declare(strict_types=1);
+
 define('PHP_START', microtime(true));
 
 require_once '/home/vallabh/.vim/vendor/autoload.php';
@@ -8,7 +10,7 @@ echo 'List down all the default constant defined by the php ';
 print_r(get_defined_constants(true));
 
 // $dt = new DateTime('2019-02-01T03:45:27+00:00');
-$dt = new DateTime();
+$dt    = new DateTime();
 $array = [];
 
 var_dump($dt);
