00cdb67ba01a086ab896d6dfa25602a037d3420a
Vallabh Kansagara <vrkansagara@gmail.com>
2021-10-24 08:24:00 +0530

    iptable script updated for the debian system

--- bin/info.sh
+++ bin/info.sh
@@ -2,7 +2,7 @@
 # set -e # This setting is telling the script to exit on a command error.
 # set -x # You refer to a noisy script.(Used to debugging)
 
-echo
+echo " "
 CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 export DEBIAN_FRONTEND=noninteractive
 
@@ -20,3 +20,16 @@ free -mh | awk '/^Mem:/ {print $3 "/" $2}'
 
 echo "Show CPU temperature"
 sensors
+
+lspci -nn | grep -i audio 
+speaker-test -t wav -c 2
+dmesg | grep snd 
+${SUDO} alsactl init
+aplay -l
+amixer
+cat /proc/asound/cards
+inxi -Fxz
+# create a 10 second output, 30 fps (300 frames total), with a frame size of 640x360 (testsrc.mpg)
+ffmpeg -f lavfi -i testsrc=duration=10:size=1280x720:rate=30 /tmp/testsrc.mpg
+
+sensors| grep -i rpm | awk '{ print "Fan "$3"/"$11" RPM"}'
