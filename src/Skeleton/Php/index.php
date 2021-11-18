5b56b88ce141537f9458d45c8e4f84658f751922
Vallabh Kansagara <vrkansagara@gmail.com>
2021-11-18 10:36:22 +0530

    adding phprun configuration

--- src/Skeleton/Php/index.php
+++ src/Skeleton/Php/index.php
@@ -10,17 +10,17 @@ echo 'List down all the default constant defined by the php ';
 print_r(get_defined_constants(true));
 
 // $dt = new DateTime('2019-02-01T03:45:27+00:00');
-$dt    = new DateTime();
+$dt = new DateTime();
 $array = [];
 
 var_dump($dt);
 
 echo sprintf(
-    '[ %s ] take %2.5f milliseconds  to complete',
-    $_SERVER['PHP_SELF'],
-    microtime(true) - PHP_START
+	'[ %s ] take %2.5f milliseconds  to complete',
+	$_SERVER['PHP_SELF'],
+	microtime(true) - PHP_START
 ) . PHP_EOL;
 
-$dt =" I am new date string";
+$dt = ' I am new date string';
 
 echo (string) "This is date $dt";
