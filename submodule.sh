e75ef8a0a2cc574d06fb99edc4ec3d7fb40f20c1
Vallabh Kansagara <vrkansagara@gmail.com>
2022-06-01 08:32:08 +0200

    removal of coc-nvim

--- submodule.sh
+++ submodule.sh
@@ -24,7 +24,7 @@ cd ${VIM_DIRECTORY}
 
 remove_vim_vendor_module(){
 
-	read -r -p 'Do you want to remove VIM vendor,php copmoser, coc vendor [Y/n]?' input
+read -r -p 'Do you want to remove VIM vendor,php copmoser, coc vendor [Y/n]?' input
 case $input in [yY][eE][sS]|[yY])
 # ${SUDO} rm -rf ~/.config/coc
 ${SUDO} rm -rf vendor/*
@@ -57,8 +57,8 @@ git submodule add -f  https://github.com/junegunn/fzf.vim pack/vendor/start/fzf.
 echo "Installation of [ commentary.vim: comment stuff out    ] ..."
 git submodule add -f https://github.com/tpope/vim-commentary.git pack/vendor/start/vim-commentary
 
-echo "Installation of [ Nodejs extension host for vim & neovim, load extensions like VSCode and host language servers. ] ..."
-git submodule add -f --branch release https://github.com/neoclide/coc.nvim.git pack/vendor/start/coc-nvim
+#echo "Installation of [ Nodejs extension host for vim & neovim, load extensions like VSCode and host language servers. ] ..."
+#git submodule add -f --branch release https://github.com/neoclide/coc.nvim.git pack/vendor/start/coc-nvim
 
 echo "Installation of [ Multiple cursors plugin for vim/neovim ] ..."
 git submodule add -f https://github.com/mg979/vim-visual-multi.git pack/vendor/start/vim-visual-multi
@@ -138,18 +138,18 @@ cd ${VIM_DIRECTORY}
 # echo "Installation of [ Go development plugin for Vim ] ..."
 # git submodule add -f https://github.com/fatih/vim-go.git --depth=1  vendor/vim-go
 
-git submodule update --init --recursive --jobs 4  --remote --merge &
+git submodule update --init --recursive --jobs 4  --remote --merge
 
 bin/composer self-update
-bin/composer install --prefer-dist --no-scripts --no-progress --no-interaction --no-dev &
+bin/composer install --prefer-dist --no-scripts --no-progress --no-interaction --no-dev
 ${SUDO} npm i -g npm@latest intelephense@latest livereloadx yarn
 yarn set version latest
 
 # update coc-nvim plugines
-echo "Wait for 2 minutes, coc-nvim plugines is started updating"
-vim -c 'CocUpdateSync|q'
-echo "Add intelephense license here"
-node -e "console.log(os.homedir() + '/intelephense/licence.txt')"
+#echo "Wait for 2 minutes, coc-nvim plugines is started updating"
+#vim -c 'CocUpdateSync|q'
+#echo "Add intelephense license here"
+#node -e "console.log(os.homedir() + '/intelephense/licence.txt')"
 
 echo "Submodule installation recursive dependence .....................[DONE]."
 
