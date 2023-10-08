c2c09439e583e295ff069f6f784c19f577757e50
Vallabh Kansagara <vrkansagara@gmail.com>
2023-10-08 09:09:41 +0530

    adding google chrome extention

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e # This setting is telling the script to exit on a command error.
 if [[ "$1" == "-v" ]]; then
-  set -x # You refer to a noisy script.(Used to debugging)
+    set -x # You refer to a noisy script.(Used to debugging)
 fi
 
 echo " "
@@ -9,16 +9,16 @@ CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 export DEBIAN_FRONTEND=noninteractive
 
 if [ "$(whoami)" != "root" ]; then
-  SUDO=sudo
+    SUDO=sudo
 fi
 
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 #  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
-#  Note		  :- Init script
+#  Note       :- Init script
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 cd /tmp
-${SUIDO} rm -rf /tmp/jq $(pwd)/bin/jq
+${SUDO} rm -rf /tmp/jq $(pwd)/bin/jq
 wget https://github.com/stedolan/jq/releases/latest/download/jq-linux64 -O jq
 chmod +x /tmp/jq
 echo '{"foo": 0}' | /tmp/jq .
@@ -26,7 +26,7 @@ mv /tmp/jq $(pwd)/bin
 
 
 cd /tmp
-${SUIDO} rm -rf /tmp/JMESPath $(pwd)/bin/JMESPath
+${SUDO} rm -rf /tmp/JMESPath $(pwd)/bin/JMESPath
 wget https://github.com/jmespath/jp/releases/latest/download/jp-linux-amd64 -O JMESPath
 chmod +x /tmp/JMESPath
 echo '{"a": "foo", "b": "bar", "c": "baz"}' | /tmp/JMESPath a
