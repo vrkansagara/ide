9e088e1956beb7a0438131cc3b72ff7f31fea803
Vallabhdas Kansagara <vrkansagara@gmail.com>
2024-03-29 14:21:56 +0530

    correction in name

--- src/Sh/Git/config.sh
+++ src/Sh/Git/config.sh
@@ -9,7 +9,7 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#  Maintainer :- Vallabh Kansagara<vrkansagara@gmail.com> — @vrkansagara
+#  Maintainer :- vallabhdas kansagara<vrkansagara@gmail.com> — @vrkansagara
 #  Note       :-
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
@@ -44,7 +44,7 @@ git config --global merge.tool vimdiff
 git config --global mergetool.prompt false
 git config --global pull.rebase false
 git config --global url."https://".insteadOf git://
-git config --global user.name "Vallabh Kansagara"
+git config --global user.name "vallabhdas kansagara"
 git config --global user.signingkey 8BA6E7ABD8112B3E
 
 git config --global alias.add-unmerged '!f() { git diff --name-status --diff-filter=U | cut -f2 ; }; git add `f`'
