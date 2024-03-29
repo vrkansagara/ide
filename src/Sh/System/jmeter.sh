7fb356bfd2129a91335aa39b5327064906ad19d0
Vallabh Kansagara <vrkansagara@gmail.com>
2024-03-29 13:37:47 +0530

    adding random changes
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- src/Sh/System/jmeter.sh
+++ src/Sh/System/jmeter.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "-v" ]]; then
 fi
 
 if [ "$(whoami)" != "root" ]; then
-	SUDO=sudo
+  SUDO=sudo
 fi
 
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -14,24 +14,23 @@ fi
 # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 cd /tmp
-JMETER_VERSION="apache-jmeter-5.5"
+JMETER_VERSION="apache-jmeter-5.6.3"
 ${SUDO} rm -rf "/tmp/$JMETER_VERSION*"
 
 wget -k "https://dlcdn.apache.org//jmeter/binaries/${JMETER_VERSION}.tgz"
 wget -k "https://www.apache.org/dist/jmeter/binaries/${JMETER_VERSION}.tgz.asc"
-gpg --keyserver pgpkeys.mit.edu --recv-key  C4923F9ABFB2F1A06F08E88BAC214CAA0612B399
+gpg --keyserver pgpkeys.mit.edu --recv-key C4923F9ABFB2F1A06F08E88BAC214CAA0612B399
 #gpg --fingerprint C4923F9ABFB2F1A06F08E88BAC214CAA0612B399
 
 gpg --verify "${JMETER_VERSION}.tgz.asc" "${JMETER_VERSION}.tgz"
 
-if [ $? -eq 0 ]
-then
-   tar -xvf "${JMETER_VERSION}.tgz"
-    mv "/tmp/${JMETER_VERSION}" /tmp/apache-jmeter
-    rm -rf "$HOME/Applications/apache-jmeter"
-    mv /tmp/apache-jmeter $HOME/Applications
+if [ $? -eq 0 ]; then
+  tar -xvf "${JMETER_VERSION}.tgz"
+  mv "/tmp/${JMETER_VERSION}" /tmp/apache-jmeter
+  rm -rf "$HOME/Applications/apache-jmeter"
+  mv /tmp/apache-jmeter $HOME/Applications
 else
-    echo Problem with signature.
+  echo Problem with signature.
 fi
 
 ${SUDO} chmod ugo+xr $HOME/Applications/apache-jmeter/bin/jmeter
