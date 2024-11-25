e8f9468c4e854cda2214b01561208d92e7fdd5e5
vallabhdas kansagara <vrkansagara@gmail.com>
2024-11-25 11:24:43 +0530

    fet: random fix

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e # This setting is telling the script to exit on a command error.
 if [[ "$1" == "-v" ]]; then
+  shift
   set -x # You refer to a noisy script.(Used to debugging)
 fi
 
-export CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 
 if [ "$(whoami)" != "root" ]; then
   SUDO=sudo
