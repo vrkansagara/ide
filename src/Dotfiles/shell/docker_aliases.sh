58d92e4afc695a3592a0f9ac2fcdc6c32f7e95af
vallabhdas kansagara <vrkansagara@gmail.com>
2024-12-11 16:51:15 +0530

    feat: adding docker with volume down

--- src/Dotfiles/shell/docker_aliases.sh
+++ src/Dotfiles/shell/docker_aliases.sh
@@ -12,6 +12,7 @@ alias dcE='docker-compose exec -u root '
 alias ds='docker-compose ps --services'
 alias dcb='docker-compose up -dV --build --remove-orphans --force-recreate '
 alias dcu='docker-compose up -dV --remove-orphans --force-recreate '
+alias dcdV='docker-compose down --volumes '
 alias dcl='docker-compose logs --follow --timestamps --tail 50 '
 alias dIps="docker ps -q | xargs -n 1 docker inspect --format '{{ .NetworkSettings.IPAddress }} {{ .Name }} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' | sed 's/ \// /'"
 alias dm="docker-machine"
