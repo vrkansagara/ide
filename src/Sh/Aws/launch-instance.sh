4ac9b3060b4a2926eb566247f984eb4ff38d4aca
Vallabhdas Kansagara <vrkansagara@gmail.com>
2024-12-24 08:52:47 +0530

    feat: adding az info into info.html

--- src/Sh/Aws/launch-instance.sh
+++ src/Sh/Aws/launch-instance.sh
@@ -12,4 +12,15 @@ ${SUDO} apt install nginx -y
 ${SUDO} systemctl enable nginx
 ${SUDO} systemctl start nginx
 
-echo "instance created on ${CURRENT_DATE}" | sudo tee /var/www/html/index.nginx-debian.html
+# Fetch the Availability Zone information using IMDSv2
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+AZ=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone`
+
+
+# Create the index.html file
+cat > /var/www/html/info.html <<EOF
+<div>This instance is located in Availability Zone: $AZ</div>
+EOF
+
+
+echo "instance created on ${CURRENT_DATE}" | ${SUDO} tee /var/www/html/index.nginx-debian.html
