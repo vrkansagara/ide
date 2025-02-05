7d4da4a973de8a6b27a3042bceca0a54b4b53278
Vallabhdas Kansagara <vrkansagara@gmail.com>
2025-02-05 12:38:30 +0530

    random changes..

---
+++ bin/args.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+
+echo "Total argument passed is :- $#"
+echo "\$# is  :- $#"
+echo "\$@ is  :- $@"
+echo "\$0 is  :- $0"
+echo "\$1 is  :- $1"
+echo "\$2 is  :- $2"
+echo "\$3 is  :- $3"
